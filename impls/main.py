0f8d24123a07ee6676991c05d4d080034bac23e3
satyam19k <satyamk1143@gmail.com>
2025-03-03 21:06:10 -0500

    pusht related changes

--- impls/main.py
+++ impls/main.py
@@ -3,7 +3,9 @@ import os
 import random
 import time
 from collections import defaultdict
-
+import torch
+import pickle
+from flax.core.frozen_dict import freeze
 import jax
 import numpy as np
 import tqdm
@@ -16,6 +18,14 @@ from utils.env_utils import make_env_and_datasets
 from utils.evaluation import evaluate
 from utils.flax_utils import restore_agent, save_agent
 from utils.log_utils import CsvLogger, get_exp_name, get_flag_dict, get_wandb_video, setup_wandb
+
+
+import gym
+import json
+import torch
+import numpy as np
+from env.venv import SubprocVectorEnv
+
 
 FLAGS = flags.FLAGS
 
@@ -39,7 +49,12 @@ flags.DEFINE_integer('video_episodes', 1, 'Number of video episodes for each tas
 flags.DEFINE_integer('video_frame_skip', 3, 'Frame skip for videos.')
 flags.DEFINE_integer('eval_on_cpu', 1, 'Whether to evaluate on CPU.')
 
-config_flags.DEFINE_config_file('agent', 'agents/gciql.py', lock_config=False)
+# config_flags.DEFINE_config_file('agent', 'agents/gciql.py', lock_config=False)
+config_flags.DEFINE_config_file('agent', 'agents/hiql.py', lock_config=False)
+
+n_evals = 10
+eval_seed = [99* n + 1 for n in range(n_evals)]
+filename = "/Users/jayesh/MSCS/RL/HIQL_pushT/ogbench_prev/impls/plots/rollout"
 
 
 def main(_):
@@ -53,26 +68,102 @@ def main(_):
     with open(os.path.join(FLAGS.save_dir, 'flags.json'), 'w') as f:
         json.dump(flag_dict, f)
 
-    # Set up environment and dataset.
+
+    def prepare_dataset(data_path,dset_type):
+        data_path = data_path + dset_type
+
+        ACTION_MEAN = torch.tensor([-0.0087, 0.0068])
+        ACTION_STD = torch.tensor([0.2019, 0.2002])
+
+        action_scale=100.0
+
+        with open(data_path + "seq_lengths.pkl" ,"rb") as f:
+            seq_lengths = pickle.load(f)
+
+        states = torch.load(data_path + "states.pth")
+        vel = torch.load(data_path + "velocities.pth")
+        actions = torch.load(data_path + "rel_actions.pth")
+
+
+        states = states.float()
+        actions = actions.float()
+        vel = vel.float()
+
+
+        states_0=states[0,:seq_lengths[0],:] 
+        vel_0 = vel[0,:seq_lengths[0],:]
+        actions_0 = actions[0,:seq_lengths[0],:]
+        actions_0 = actions_0 / action_scale
+        
+
+        observations = torch.cat([states_0.float(), vel_0.float()], dim=-1)
+        actions_dt = (actions_0 - ACTION_MEAN) / ACTION_STD
+
+        terminals = torch.zeros(observations.shape[0], dtype=torch.float32)
+        valids = torch.ones(observations.shape[0], dtype=torch.float32)
+        terminals[-2:] = 1.0
+        valids[-1] = 0.0
+
+
+        for i in range(1,states.shape[0]):
+            seq_len = seq_lengths[i]
+
+            states_0=states[0,:seq_len,:] 
+            vel_0 = vel[0,:seq_len,:]
+
+            
+            actions_0 = actions[0,:seq_len,:]
+            actions_0 = actions_0 / action_scale
+            
+
+            observations_i = torch.cat([states_0.float(), vel_0.float()], dim=-1)
+            actions_i = (actions_0 - ACTION_MEAN) / ACTION_STD
+
+            observations = torch.cat((observations, observations_i), dim=0)
+            actions_dt = torch.cat((actions_dt, actions_i), dim=0)
+
+            terminals_i = torch.zeros(observations_i.shape[0], dtype=torch.float32)
+            valids_i = torch.ones(observations_i.shape[0], dtype=torch.float32)
+            terminals_i[-2:] = 1.0
+            valids_i[-1] = 0.0
+
+            terminals = torch.cat((terminals, terminals_i), dim=0)
+            valids = torch.cat((valids, valids_i), dim=0)
+        
+        print(observations.shape)
+        print(actions_dt.shape)
+
+        data_dict = {
+            'terminals': terminals.detach().cpu().numpy(),
+            'valids': valids.detach().cpu().numpy(),
+            'observations': observations.detach().cpu().numpy(),
+            'actions': actions_dt.detach().cpu().numpy()
+        }
+        return freeze(data_dict)
+
+
+
+    data_path = "/Users/jayesh/MSCS/RL/HIQL_pushT/pusht_noise/"
+
+    train_dataset_dict = prepare_dataset(data_path,"train/")
+    val_dataset_dict = prepare_dataset(data_path,"val/")
+
+
     config = FLAGS.agent
-    env, train_dataset, val_dataset = make_env_and_datasets(FLAGS.env_name, frame_stack=config['frame_stack'])
 
     dataset_class = {
         'GCDataset': GCDataset,
         'HGCDataset': HGCDataset,
     }[config['dataset_class']]
-    train_dataset = dataset_class(Dataset.create(**train_dataset), config)
-    if val_dataset is not None:
-        val_dataset = dataset_class(Dataset.create(**val_dataset), config)
+    train_dataset = dataset_class(Dataset.create(**train_dataset_dict), config)
+    if val_dataset_dict is not None:
+        val_dataset = dataset_class(Dataset.create(**val_dataset_dict), config)
 
-    # Initialize agent.
     random.seed(FLAGS.seed)
     np.random.seed(FLAGS.seed)
 
-    example_batch = train_dataset.sample(1)
-    if config['discrete']:
-        # Fill with the maximum action to let the agent know the action space size.
-        example_batch['actions'] = np.full_like(example_batch['actions'], env.action_space.n - 1)
+    example_batch,_ = train_dataset.sample(1)
+
 
     agent_class = agents[config['agent_name']]
     agent = agent_class.create(
@@ -82,18 +173,16 @@ def main(_):
         config,
     )
 
-    # Restore agent.
     if FLAGS.restore_path is not None:
         agent = restore_agent(agent, FLAGS.restore_path, FLAGS.restore_epoch)
 
-    # Train agent.
     train_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'train.csv'))
     eval_logger = CsvLogger(os.path.join(FLAGS.save_dir, 'eval.csv'))
     first_time = time.time()
     last_time = time.time()
     for i in tqdm.tqdm(range(1, FLAGS.train_steps + 1), smoothing=0.1, dynamic_ncols=True):
         # Update agent.
-        batch = train_dataset.sample(config['batch_size'])
+        batch,idxs = train_dataset.sample(config['batch_size'])
         agent, update_info = agent.update(batch)
 
         # Log metrics.
@@ -115,39 +204,43 @@ def main(_):
                 eval_agent = jax.device_put(agent, device=jax.devices('cpu')[0])
             else:
                 eval_agent = agent
-            renders = []
             eval_metrics = {}
             overall_metrics = defaultdict(list)
-            task_infos = env.unwrapped.task_infos if hasattr(env.unwrapped, 'task_infos') else env.task_infos
-            num_tasks = FLAGS.eval_tasks if FLAGS.eval_tasks is not None else len(task_infos)
-            for task_id in tqdm.trange(1, num_tasks + 1):
-                task_name = task_infos[task_id - 1]['task_name']
-                eval_info, trajs, cur_renders = evaluate(
-                    agent=eval_agent,
-                    env=env,
-                    task_id=task_id,
-                    config=config,
-                    num_eval_episodes=FLAGS.eval_episodes,
-                    num_video_episodes=FLAGS.video_episodes,
-                    video_frame_skip=FLAGS.video_frame_skip,
-                    eval_temperature=FLAGS.eval_temperature,
-                    eval_gaussian=FLAGS.eval_gaussian,
-                )
-                renders.extend(cur_renders)
-                metric_names = ['success']
-                eval_metrics.update(
-                    {f'evaluation/{task_name}_{k}': v for k, v in eval_info.items() if k in metric_names}
-                )
-                for k, v in eval_info.items():
-                    if k in metric_names:
-                        overall_metrics[k].append(v)
+            env = SubprocVectorEnv(
+                [
+                    lambda: gym.make(
+                        "pusht", [], {'with_velocity': True, 'with_target': True}
+                    )
+                    for _ in range(n_evals)
+                ]
+            )
+
+            eval_results = evaluate(
+                agent=eval_agent,
+                env=env,
+                traj_len=26,
+                idxs=idxs,
+                train_dataset_dict=train_dataset_dict,
+                n_evals=n_evals,
+                filename=filename,
+                config=config,
+                batch = batch,
+                seeds=eval_seed,
+                eval_temperature=FLAGS.eval_temperature,
+                eval_gaussian=FLAGS.eval_gaussian,
+            )
+            # renders.extend(cur_renders)
+            metric_names = ['success']
+            eval_metrics.update(
+                {f'evaluation/{"test_eval"}_{k}': v for k, v in eval_results.items()}
+            )
+
+            for k, v in eval_results.items():
+                if k in metric_names:
+                    overall_metrics[k].append(v)
+
             for k, v in overall_metrics.items():
                 eval_metrics[f'evaluation/overall_{k}'] = np.mean(v)
-
-            if FLAGS.video_episodes > 0:
-                video = get_wandb_video(renders=renders, n_cols=num_tasks)
-                eval_metrics['video'] = video
-
             wandb.log(eval_metrics, step=i)
             eval_logger.log(eval_metrics, step=i)
 
--- impls/utils/evaluation.py
+++ impls/utils/evaluation.py
@@ -3,7 +3,13 @@ from collections import defaultdict
 import jax
 import numpy as np
 from tqdm import trange
-
+import torch
+import random
+from preprocessor import Preprocessor
+from torchvision import utils
+from env.pusht.pusht_wrapper import PushTWrapper
+from einops import rearrange, repeat
+import imageio
 
 def supply_rng(f, rng=jax.random.PRNGKey(0)):
     """Helper function to split the random number generator key before each call to the function."""
@@ -37,13 +43,20 @@ def add_to(dict_of_lists, single_dict):
 def evaluate(
     agent,
     env,
-    task_id=None,
+    batch,
+    seeds,
+    traj_len,
+    idxs,
+    train_dataset_dict,
+    n_evals,
+    filename,
     config=None,
     num_eval_episodes=50,
     num_video_episodes=0,
     video_frame_skip=3,
     eval_temperature=0,
     eval_gaussian=None,
+    
 ):
     """Evaluate the agent in the environment.
 
@@ -61,57 +74,169 @@ def evaluate(
     Returns:
         A tuple containing the statistics, trajectories, and rendered videos.
     """
+
+    ACTION_MEAN = torch.tensor([-0.0087, 0.0068])
+    ACTION_STD = torch.tensor([0.2019, 0.2002])
+    STATE_MEAN = torch.tensor([236.6155, 264.5674, 255.1307, 266.3721, 1.9584, -2.93032027,  2.54307914])
+    STATE_STD = torch.tensor([101.1202, 87.0112, 52.7054, 57.4971, 1.7556, 74.84556075, 74.14009094])
+    PROPRIO_MEAN = torch.tensor([236.6155, 264.5674, -2.93032027,  2.54307914])
+    PROPRIO_STD = torch.tensor([101.1202, 87.0112, 74.84556075, 74.14009094])
+    data_preprocessor = Preprocessor(
+            action_mean=ACTION_MEAN,
+            action_std=ACTION_STD,
+            state_mean=STATE_MEAN,
+            state_std=STATE_STD,
+            proprio_mean=PROPRIO_MEAN,
+            proprio_std=PROPRIO_STD,
+            transform="datasets.img_transforms.default_transform",
+        )
+    
+
     actor_fn = supply_rng(agent.sample_actions, rng=jax.random.PRNGKey(np.random.randint(0, 2**32)))
-    trajs = []
-    stats = defaultdict(list)
 
-    renders = []
-    for i in trange(num_eval_episodes + num_video_episodes):
-        traj = defaultdict(list)
-        should_render = i >= num_eval_episodes
+    video_lengths = []
+    video_starts = []
 
-        observation, info = env.reset(options=dict(task_id=task_id, render_goal=should_render))
-        goal = info.get('goal')
-        goal_frame = info.get('goal_rendered')
-        done = False
-        step = 0
-        render = []
-        while not done:
-            action = actor_fn(observations=observation, goals=goal, temperature=eval_temperature)
-            action = np.array(action)
-            if not config.get('discrete'):
-                if eval_gaussian is not None:
-                    action = np.random.normal(action, eval_gaussian)
-                action = np.clip(action, -1, 1)
 
-            next_observation, reward, terminated, truncated, info = env.step(action)
-            done = terminated or truncated
-            step += 1
+    for k in range(len(idxs)):
+        current_start = idxs[k]
+        for i, x in enumerate(train_dataset_dict['valids'][idxs[k]:]):
+            if x == 0.0:
+                segment_length = i - current_start + 1
+                video_lengths.append(segment_length)
+                video_starts.append(current_start)
+                break;
 
-            if should_render and (step % video_frame_skip == 0 or done):
-                frame = env.render().copy()
-                if goal_frame is not None:
-                    render.append(np.concatenate([goal_frame, frame], axis=0))
-                else:
-                    render.append(frame)
 
-            transition = dict(
-                observation=observation,
-                next_observation=next_observation,
-                action=action,
-                reward=reward,
-                done=done,
-                info=info,
+    filtered_lengths = []
+    filtered_starts = []
+    for length, start in zip(video_lengths, video_starts):
+        if length >= traj_len:
+            filtered_lengths.append(length)
+            filtered_starts.append(start)
+    
+    
+    states = []
+    actions = []
+    for i in range(n_evals):
+        traj_id = random.randint(0, len(filtered_starts) - 1)
+
+        max_offset = filtered_lengths[traj_id]- traj_len
+
+        offset = random.randint(0, max_offset)
+
+        state = train_dataset_dict['observations'][filtered_starts[traj_id]+offset : filtered_starts[traj_id]+offset+traj_len]
+        action = train_dataset_dict['actions'][filtered_starts[traj_id]+offset : filtered_starts[traj_id]+offset+traj_len-1]
+        actions.append(torch.from_numpy(action))
+        states.append(state)
+
+    init_state = [x[0] for x in states]
+    init_state = np.array(init_state)
+    actions = torch.stack(actions)
+    exec_actions = data_preprocessor.denormalize_actions(actions)
+
+    rollout_obses_gt, rollout_states_gt = env.rollout(seeds, init_state, exec_actions.numpy())
+
+    obs_0 = {
+    key: np.expand_dims(arr[:, 0], axis=1)
+    for key, arr in rollout_obses_gt.items()
+}
+    obs_g = {
+        key: np.expand_dims(arr[:, -1], axis=1)
+        for key, arr in rollout_obses_gt.items()
+    }
+    state_0 = init_state  # (b, d)
+    state_g = rollout_states_gt[:, -1]  # (b, d)
+
+    act=[]
+    final_state = np.array([156.6379, 357.0037, 251.5347, 260.9974,   0.7817,  22.8611, -26.2061])
+
+    for k in range(n_evals):
+        in_st = init_state[k]
+        res=[]
+        for i in range(traj_len):
+            action = actor_fn(observations=in_st, goals=final_state, temperature=eval_temperature)
+            res.append(action)
+        actions_model = torch.stack([torch.from_numpy(np.asarray(x)) for x in res], dim=0)
+        act.append(actions_model)
+    
+    act = torch.stack(act)
+    exec_actions_model = data_preprocessor.denormalize_actions(act)
+
+    e_obses, e_states = env.rollout(seeds, init_state, exec_actions_model)
+
+    eval_results = env.eval_state(state_g, e_states[:,-1])
+    successes = eval_results['success']
+
+    visual_dists = np.linalg.norm(e_obses["visual"] - obs_g["visual"], axis=1)
+    mean_visual_dist = np.mean(visual_dists)
+
+    eval_results['visual_dists'] = mean_visual_dist
+
+    e_visuals = e_obses["visual"]
+
+    e_visuals = data_preprocessor.transform_obs_visual(e_visuals)
+    result = e_visuals.clone()  # Clone to preserve the original tensor
+
+
+    e_visuals = e_visuals[: n_evals]
+    goal_visual = obs_g["visual"][: n_evals]
+    goal_visual = data_preprocessor.transform_obs_visual(goal_visual)
+
+    correction = 0.3  # to distinguish env visuals and imagined visuals
+    
+
+    for idx in range(e_visuals.shape[0]):
+        success_tag = "success" if successes[idx] else "failure"
+        frames = []
+        for i in range(e_visuals.shape[1]):
+            e_obs = e_visuals[idx, i, ...]
+            e_obs = torch.cat(
+                [e_obs.cpu(), goal_visual[idx, 0] - correction], dim=2
             )
-            add_to(traj, transition)
-            observation = next_observation
-        if i < num_eval_episodes:
-            add_to(stats, flatten(info))
-            trajs.append(traj)
-        else:
-            renders.append(np.array(render))
+            frame = e_obs
+            frame = rearrange(frame, "c w1 w2 -> w1 w2 c")
+            frame = rearrange(frame, "w1 w2 c -> (w1) w2 c")
+            frame = frame.detach().cpu().numpy()
+            frames.append(frame)
+        video_writer = imageio.get_writer(
+            f"{filename}_{idx}_{success_tag}.mp4", fps=12
+        )
 
-    for k, v in stats.items():
-        stats[k] = np.mean(v)
+        for frame in frames:
+            frame = frame * 2 - 1 if frame.min() >= 0 else frame
+            video_writer.append_data(
+                (((np.clip(frame, -1, 1) + 1) / 2) * 255).astype(np.uint8)
+            )
+        video_writer.close()
 
-    return stats, trajs, renders
+    
+    e_visuals = e_visuals[:, :: 5]
+
+
+    n_columns = e_visuals.shape[1]
+
+    e_visuals = torch.cat([e_visuals.cpu(), goal_visual - correction], dim=1)
+    rollout = e_visuals
+ 
+    n_columns += 1
+
+    imgs_for_plotting = rearrange(rollout, "b h c w1 w2 -> (b h) c w1 w2")
+    imgs_for_plotting = (
+        imgs_for_plotting * 2 - 1
+        if imgs_for_plotting.min() >= 0
+        else imgs_for_plotting
+    )
+    utils.save_image(
+        imgs_for_plotting,
+        f"{filename}.png",
+        nrow=n_columns, 
+        normalize=True,
+        value_range=(-1, 1),
+    )
+
+
+    return eval_results
+
+
+
